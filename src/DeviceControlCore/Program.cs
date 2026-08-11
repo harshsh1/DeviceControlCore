090724f8aeaa05552b4451d018e9ffe780576bf1
hasrsh1 <hs6682100@gmail.com>
2026-08-11 17:58:21 +0530

    Register Services in Program.cs

--- src/DeviceControlCore/Program.cs
+++ src/DeviceControlCore/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DeviceControlCore.Options;
+using DeviceControlCore.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+var builder = Host.CreateApplicationBuilder(new HostApplicationBuilderSettings
+{
+	Args = args,
+	ContentRootPath = AppContext.BaseDirectory
+});
+
+Log.Logger = new LoggerConfiguration()
+	.ReadFrom.Configuration(builder.Configuration)
+	.Enrich.FromLogContext()
+	.CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Services.AddSerilog();
+
+builder.Services.Configure<ServiceOptions>(
+	builder.Configuration.GetSection(ServiceOptions.SectionName));
+
+builder.Services.AddSingleton<IStateService, StateService>();
+builder.Services.AddSingleton<IPreInstallScriptRunner, PreInstallScriptRunner>();
+builder.Services.AddSingleton<IUpdateService, UpdateService>();
+builder.Services.AddSingleton<IOsSettingsService, OsSettingsService>();
+
+builder.Services.AddSingleton<DeviceMonitorService>();
+builder.Services.AddSingleton<IDeviceMonitorService>(sp => sp.GetRequiredService<DeviceMonitorService>());
+builder.Services.AddHostedService(sp => sp.GetRequiredService<DeviceMonitorService>());
+
+builder.Services.AddHostedService<ConsoleHostedService>();
+
+var host = builder.Build();
+
+try
+{
+	await host.RunAsync();
+}
+finally
+{
+	Log.CloseAndFlush();
+}
